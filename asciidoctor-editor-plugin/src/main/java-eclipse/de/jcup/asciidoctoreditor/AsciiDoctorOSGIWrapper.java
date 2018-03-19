b3c66cca9ba563c406dd211f605b66cd5f2b1544
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2018-03-19 22:08:03 +0100

    Divided AsciiDoctor Access and OSGI Wrapper

--- asciidoctor-editor-plugin/src/main/java-eclipse/de/jcup/asciidoctoreditor/AsciiDoctorOSGIWrapper.java
+++ asciidoctor-editor-plugin/src/main/java-eclipse/de/jcup/asciidoctoreditor/AsciiDoctorOSGIWrapper.java
@@ -3,19 +3,10 @@ package de.jcup.asciidoctoreditor;
 import static org.asciidoctor.Asciidoctor.Factory.*;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.Arrays;
-import java.util.Map;
 
-import org.apache.commons.io.IOUtils;
 import org.asciidoctor.Asciidoctor;
-import org.asciidoctor.Attributes;
-import org.asciidoctor.AttributesBuilder;
-import org.asciidoctor.OptionsBuilder;
-import org.asciidoctor.SafeMode;
 import org.eclipse.core.runtime.Platform;
 import org.jruby.RubyInstanceConfig;
 import org.jruby.javasupport.JavaEmbedUtils;
@@ -51,20 +42,19 @@ public class AsciiDoctorOSGIWrapper {
 	private static final String LIBS_PLUGIN_ID = "de.jcup.asciidoctoreditor.libs";
 	public static final AsciiDoctorOSGIWrapper INSTANCE = new AsciiDoctorOSGIWrapper();
 	private Asciidoctor asciidoctor;
-	private Path tempFolder;
-	private EclipseResourceHelper helper;
-
+	
 	private AsciiDoctorOSGIWrapper() {
-		helper = EclipseResourceHelper.DEFAULT;
-
 		/* load asciidoctor OSGI conform */
 		Bundle bundle = Platform.getBundle(LIBS_PLUGIN_ID);
 		String versionName = bundle.getVersion().toString();
 		ClassLoader libsClassLoader = fetchClassLoader(bundle);
 		
 		initAscIIDoctor(versionName, libsClassLoader);
-		initTempFolderOrFail();
 		asciidoctor = create(libsClassLoader);
+	}
+	
+	public Asciidoctor getAsciidoctor() {
+		return asciidoctor;
 	}
 
 	protected void initAscIIDoctor(String versionName, ClassLoader libsClassLoader) {
@@ -74,14 +64,6 @@ public class AsciiDoctorOSGIWrapper {
 		initializeRubyConfig(versionName, config);
 	}
 
-	protected void initTempFolderOrFail() {
-		try {
-			tempFolder = Files.createTempDirectory("ascii-doctor-eclipse");
-			tempFolder.toFile().deleteOnExit();
-		} catch (IOException e) {
-			throw new IllegalStateException("Not able to provide tempfolder",e);
-		}
-	}
 
 	private void initializeRubyConfig(String versionName, RubyInstanceConfig config) {
 		File unzippedGEMSfolder = ensureUnzippedRubGemsArtefactsAvailable(versionName);
@@ -158,91 +140,5 @@ public class AsciiDoctorOSGIWrapper {
 		support.unzip(zipFile, unzippedGEMSfolder);
 	}
 
-	public String convertToHTML(String asciiDoc) {
-		String html = asciidoctor.convert(asciiDoc, getDefaultOptions());
-		StringBuilder sb = new StringBuilder();
-		sb.append("<html>");
-		sb.append("<head>");
-
-		sb.append("<style>");
-		File defaultCSSFileInPlugin;
-		File codeRayCSSFileInPlugin;
-		try{
-			defaultCSSFileInPlugin = helper.getFileInPlugin("css/default.css");
-			codeRayCSSFileInPlugin = helper.getFileInPlugin("css/coderay.css");
-			
-		}catch(IOException e){
-			String message = "NO css data available! Cannot render files";
-			AsciiDoctorEditorUtil.logError(message,e);
-			return message;
-		}
-		if (defaultCSSFileInPlugin==null || codeRayCSSFileInPlugin==null){
-			String message = "NO css data found in plugins! Cannot render files";
-			AsciiDoctorEditorUtil.logError(message,null);
-			return message;
-		}
-		
-		try (FileInputStream defaultFOS = new FileInputStream(defaultCSSFileInPlugin);
-				FileInputStream coderayFOS = new FileInputStream(codeRayCSSFileInPlugin)) {
-			/*
-			 * adopted from
-			 * https://github.com/asciidoctor/asciidoctor-intellij-plugin :
-			 */
-			String myInlineCss = IOUtils.toString(defaultFOS);
-			// myInlineCssDarcula = myInlineCss +
-			// IOUtils.toString(JavaFxHtmlPanel.class.getResourceAsStream("darcula.css"));
-			// myInlineCssDarcula +=
-			// IOUtils.toString(JavaFxHtmlPanel.class.getResourceAsStream("coderay-darcula.css"));
-			myInlineCss += IOUtils.toString(coderayFOS);
-			sb.append(myInlineCss);
-
-		} catch (IOException e) {
-			String message = "Was not able load css data. Cannot render file.";
-			AsciiDoctorEditorUtil.logError(message,e);
-			return message;
-		}
-
-		sb.append("</style>");
-		try {
-			/*
-			 * FIXME ATR, 15.03.2018: replacwe the regexp replaceAll with static
-			 * one (at least)
-			 */
-			File fontAwesomeCSSfile = helper.getFileInPlugin("css/font-awesome/css/font-awesome.min.css");
-			String fontAwesomeCssPath = fontAwesomeCSSfile.toURI().toURL().toExternalForm();// fontAwesomeCSSfile.getAbsolutePath().replaceAll("\\\\",
-																							// "/"
-			/* FIXME ATR, 19.03.2018: remove sysout when stable */																	// );
-			System.out.println(fontAwesomeCssPath);
-			String fontAwesomeCssLink = "<link rel=\"stylesheet\" href=\"" + fontAwesomeCssPath + "\">";
-			sb.append(fontAwesomeCssLink);
-
-			File dejavouFile = helper.getFileInPlugin("css/dejavu/dejavu.css");
-			String dejavouPath = dejavouFile.getAbsolutePath().replaceAll("\\\\", "/");
-			String dejavuCssLink = "<link rel=\"stylesheet\" href=\"" + dejavouPath + "\">";
-			sb.append(dejavuCssLink);
-		} catch (IOException e) {
-			String message = "Was not able load additional css data. Cannot render file.";
-			AsciiDoctorEditorUtil.logError(message,e);
-			return message;
-		}
-		sb.append("</head>");
-		sb.append("<body>");
-		sb.append(html);
-		sb.append("</body>");
-		sb.append("</html>");
-
-		return sb.toString();
-	}
-
-	private Map<String, Object> getDefaultOptions() {
-		Attributes attrs = AttributesBuilder.attributes().showTitle(true).sourceHighlighter("coderay")
-				.attribute("coderay-css", "style").attribute("env", "eclipse").attribute("env-eclipse").get();
-		if (tempFolder != null) {
-			System.out.println("Tempfolder:" + tempFolder);
-			attrs.setAttribute("outdir", tempFolder.toAbsolutePath().normalize().toString());
-		}
-		OptionsBuilder opts = OptionsBuilder.options().safe(SafeMode.UNSAFE).backend("html5").headerFooter(false)
-				.attributes(attrs).option("sourcemap", "true").baseDir(new File("."));
-		return opts.asMap();
-	}
+	
 }
