47a107e547cfaabe8530e1a477d9d80242b592de
Albert Tregnaghi <albert.tregnaghi@gmail.com>
2018-04-19 23:44:05 +0200

    Deployment 0.6.0 #35
    - bugfix on apply_copyright script
    - Updated version to 0.6.0
    - added missing copyright headers

--- asciidoctor-editor-plugin/src/main/java-eclipse/de/jcup/asciidoctoreditor/toolbar/ChangeLayoutAction.java
+++ asciidoctor-editor-plugin/src/main/java-eclipse/de/jcup/asciidoctoreditor/toolbar/ChangeLayoutAction.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Albert Tregnaghi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ *
+ */
 package de.jcup.asciidoctoreditor.toolbar;
 
 import org.eclipse.jface.action.Action;
@@ -10,7 +25,6 @@ import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 
 import de.jcup.asciidoctoreditor.AsciiDoctorEditor;
-import de.jcup.asciidoctoreditor.PreviewLayout;
 
 public class ChangeLayoutAction extends ToolbarAction implements IMenuCreator {
 
